65fa507523eea131c12ca56c1bf8701995d35108
dmscn <ldamasceno.dev@gmail.com>
2022-07-17 12:34:47 -0300

    config(products-mfe): babel webpack setup

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,10 +1,34 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 module.exports = {
   mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/bundle'),
+    filename: 'index_bundle.js'
+  },
   devServer: {
     port: 8081,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+    ]
   },
   plugins: [
     new ModuleFederationPlugin({
