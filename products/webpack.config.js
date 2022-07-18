6b495b061eb93f3f59087738e023f0a3c028747d
dmscn <ldamasceno.dev@gmail.com>
2022-07-17 21:47:03 -0300

    chore(products-mfe): eslint setup + chakra UI

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,45 +1,55 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin")
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, '/bundle'),
-    filename: 'index_bundle.js'
-  },
-  devServer: {
-    port: 8081,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)?$/,
-        exclude: '/node_modules/',
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        },
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: ['file-loader'],
-      },
-    ]
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'products',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './Products': './src/index',
-      },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    })
-  ]
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "/bundle"),
+		filename: "index_bundle.js"
+	},
+	devServer: {
+		port: 8081,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(jsx|js)?$/,
+				exclude: "/node_modules/",
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				},
+			},
+			{
+				test: /\.(png|jp(e*)g|svg|gif)$/,
+				use: ["file-loader"],
+			},
+			{
+				test: /\.(jsx|js)?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "eslint-loader",
+				}
+			}
+		]
+	},
+	resolve: {
+		extensions: [".js", ".jsx"]
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "products",
+			filename: "remoteEntry.js",
+			exposes: {
+				"./Products": "./src/index",
+			},
+		}),
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		})
+	]
 }
